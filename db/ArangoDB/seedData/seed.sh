ac1d9b5a8842b8bf1590c174363d9b5fa0f96789
lucyyxc <xilucychen@gmail.com>
2020-12-09 21:42:44 -0800

    altered generators to create json files and shell command to seed

--- db/ArangoDB/seedData/seed.sh
+++ db/ArangoDB/seedData/seed.sh
@@ -1,9 +1,9 @@
-arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoUsersData.csv" --type csv --collection "users" --server.database "airbnb";
+# arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoUsersData.csv" --type csv --collection "users" --server.database "airbnb";
 
-arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoListingsData.csv" --type csv --collection "listings" --server.database "airbnb";
+# arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoListingsData.json" --type json --collection "listings" --create-collection true --batch-size 33554432 --progress true --threads 4 --server.database "airbnb";
 
-arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoUserListsData.csv" --type csv --collection "userLists" --server.database "airbnb";
+# arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoUserListsData.csv" --type csv --collection "userLists" --server.database "airbnb";
 
-arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoEdgeFromUsersData.csv" --type csv --collection "edgeFromUsers" --server.database "airbnb";
+# arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoEdgeFromUsersData.json" --type json --collection "edgeFromUsers" --server.database "airbnb";
 
-arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoEdgeFromUserListsData.csv" --type csv --collection "edgeFromUserLists" --server.database "airbnb";
+# arangoimport --file "/Users/lucychen/Hack Reactor/photo-carousel-service/db/ArangoDB/data/arangoEdgeFromUserListsData.json" --type json --collection "edgeFromUserLists" --server.database "airbnb";
