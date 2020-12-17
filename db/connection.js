0f1307e8ab652cc82c1a7f975c61443e25be941f
lucyyxc <xilucychen@gmail.com>
2020-12-17 13:36:51 -0800

    changed connection and query to psql

--- db/connection.js
+++ db/connection.js
@@ -1,34 +1,58 @@
-const mongoose = require('mongoose');
+const Pool = require('pg').Pool;
 
-mongoose.connect('mongodb://localhost/photoCarousel', { useNewUrlParser: true, useUnifiedTopology: true });
+const pool = new Pool({
+  user: 'postgres',
+  host: '13.57.42.153', // change it to ip address of VM
+  database: 'airbnb',
+  password: 'mypass',
+  port: 5432
+})
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('connected to mongoose!'));
+pool.connect(function(err) {
+  if (err) {
+    console.log('err connecting:' + err);
+    return;
+  }
+  console.log('connected to psql!');
+})
 
-const photoCarouselSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    unique: true,
-  },
-  listingId: Number,
-  photo: String,
-  description: String,
-  listingName: String,
-  listingStars: Number,
-  listingNumReviews: Number,
-  listingLocation: String,
-});
+module.exports.pool = pool;
 
-const userFavoritesSchema = new mongoose.Schema({
-  userId: Number,
-  listName: String,
-  favoriteLists: Array,
-  favoritePicture: String,
-});
 
-const PhotoCarousel = mongoose.model('photoCarousel', photoCarouselSchema);
-const UserFavorite = mongoose.model('userFavorites', userFavoritesSchema);
 
-module.exports.PhotoCarousel = PhotoCarousel;
-module.exports.UserFavorite = UserFavorite;
+
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/photoCarousel', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => console.log('connected to mongoose!'));
+
+// const photoCarouselSchema = new mongoose.Schema({
+//   id: {
+//     type: Number,
+//     unique: true,
+//   },
+//   listingId: Number,
+//   photo: String,
+//   description: String,
+//   listingName: String,
+//   listingStars: Number,
+//   listingNumReviews: Number,
+//   listingLocation: String,
+// });
+
+// const userFavoritesSchema = new mongoose.Schema({
+//   userId: Number,
+//   listName: String,
+//   favoriteLists: Array,
+//   favoritePicture: String,
+// });
+
+// const PhotoCarousel = mongoose.model('photoCarousel', photoCarouselSchema);
+// const UserFavorite = mongoose.model('userFavorites', userFavoritesSchema);
+
+// module.exports.PhotoCarousel = PhotoCarousel;
+// module.exports.UserFavorite = UserFavorite;
--- db/controller.js
+++ db/controller.js
@@ -1,44 +1,45 @@
 const db = require('./connection.js');
 
 module.exports = {
-  getPhotos: (req, res) => {
-    db.PhotoCarousel.find({ listingId: req.params.id }, (err, results) => {
+  getPhotos: (req, res, next) => {
+    db.pool.query('SELECT * FROM listings WHERE listingId = $1', [req.params.id], (err, results) => {
       if (err) {
+        console.log('err getting photos', err);
         res.sendStatus(404);
       } else {
-        res.json(results);
+        res.status(200).json(results.rows);
       }
     });
   },
 
-  getFavorites: (req, res) => {
-    db.UserFavorite.find({ userId: req.params.userId }, (err, results) => {
-      if (err) {
-        res.sendStatus(404);
-      } else {
-        res.json(results);
-      }
-    });
-  },
+  // getFavorites: (req, res) => {
+  //   db.UserFavorite.find({ userId: req.params.userId }, (err, results) => {
+  //     if (err) {
+  //       res.sendStatus(404);
+  //     } else {
+  //       res.json(results);
+  //     }
+  //   });
+  // },
 
-  postFavorite: (req, res) => {
-    db.UserFavorite.create(req.body, (err, results) => {
-      if (err) {
-        res.sendStatus(400);
-      } else {
-        res.json(results);
-      }
-    });
-  },
+  // postFavorite: (req, res) => {
+  //   db.UserFavorite.create(req.body, (err, results) => {
+  //     if (err) {
+  //       res.sendStatus(400);
+  //     } else {
+  //       res.json(results);
+  //     }
+  //   });
+  // },
 
-  updateFavorite: (req, res) => {
-    const { userId, listName } = req.body;
-    db.UserFavorite.updateOne({ userId, listName }, req.body, (err) => {
-      if (err) {
-        res.sendStatus(400);
-      } else {
-        res.json(req.body);
-      }
-    });
-  },
+  // updateFavorite: (req, res) => {
+  //   const { userId, listName } = req.body;
+  //   db.UserFavorite.updateOne({ userId, listName }, req.body, (err) => {
+  //     if (err) {
+  //       res.sendStatus(400);
+  //     } else {
+  //       res.json(req.body);
+  //     }
+  //   });
+  // },
 };
